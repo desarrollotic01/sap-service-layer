b5a27d2fb7a8dce70b1d5654ef845e1024236a4b
MarcoAntonioH <1386573@senati.pe>
2026-03-27 12:54:13 -0500

    correo personal

--- models/PersonalCorreo.js
+++ models/PersonalCorreo.js
@@ -1,88 +1,13 @@
-const { DataTypes } = require("sequelize");
-
 module.exports = (sequelize) => {
-  const SolicitudAlmacenLinea = sequelize.define(
-    "SolicitudAlmacenLinea",
-    {
-      id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue: DataTypes.UUIDV4,
-      },
-
-      itemId: {
-        type: DataTypes.UUID,
-        allowNull: true,
-      },
-
-      itemCode: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-
-      description: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: "",
-      },
-
-      quantity: {
-        type: DataTypes.DECIMAL(15, 2),
-        allowNull: false,
-      },
-
-      warehouseCode: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-
-      costingCode: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-
-      projectCode: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-
-      rubroId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-      },
-
-      paqueteTrabajoId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-      },
+  const PersonalCorreo = sequelize.define("PersonalCorreo", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
     },
-    {
-      tableName: "SolicitudesAlmacenLineas",
-      timestamps: false,
-    }
-  );
+    nombre: DataTypes.STRING,
+    correo: DataTypes.STRING,
+  });
 
-  SolicitudAlmacenLinea.associate = (db) => {
-    SolicitudAlmacenLinea.belongsTo(db.SolicitudAlmacen, {
-      foreignKey: "solicitud_almacen_id",
-      as: "solicitud",
-    });
-
-    SolicitudAlmacenLinea.belongsTo(db.Item, {
-      foreignKey: "itemId",
-      as: "item",
-    });
-
-   SolicitudAlmacenLinea.belongsTo(db.SapPaqueteTrabajo, {
-  foreignKey: "paqueteTrabajoId",
-  as: "paqueteTrabajo",
-});
-
-SolicitudAlmacenLinea.belongsTo(db.SapRubro, {
-  foreignKey: "rubroId",
-  as: "rubro",
-});
-  };
-
-  return SolicitudAlmacenLinea;
+  return PersonalCorreo; // 🔥 IMPORTANTE
 };
