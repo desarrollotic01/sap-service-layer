f21e3245a9d373a5b4a13e406ab594a8b11e7221
MarcoAntonioH <1386573@senati.pe>
2026-04-22 18:10:36 -0500

    changes scripts

--- scripts/seed.trabajadores.js
+++ scripts/seed.trabajadores.js
@@ -70,127 +70,127 @@ async function seedBase() {
     console.log("🧩 Familias OK");
 
   
-    /* =========================
-       TRABAJADORES
-    ========================= */
-    const trabajadores = [
-      {
-        nombre: "Luis",
-        apellido: "Torres",
-        dni: "45678912",
-        rol: "tecnico_electrico",
-        empresa: "Interno",
-      },
-      {
-        nombre: "Mario",
-        apellido: "Ramírez",
-        dni: "45678913",
-        rol: "tecnico_electrico",
-        empresa: "Interno",
-      },
-      {
-        nombre: "Lucio",
-        apellido: "Fernández",
-        dni: "45678914",
-        rol: "tecnico_electrico",
-        empresa: "Interno",
-      },
-      {
-        nombre: "Victor",
-        apellido: "Santos",
-        dni: "45678915",
-        rol: "tecnico_electrico",
-        empresa: "Interno",
-      },
-      {
-        nombre: "Aldair",
-        apellido: "Mendoza",
-        dni: "45678916",
-        rol: "tecnico_electrico",
-        empresa: "Interno",
-      },
+    // /* =========================
+    //    TRABAJADORES
+    // ========================= */
+    // const trabajadores = [
+    //   {
+    //     nombre: "Luis",
+    //     apellido: "Torres",
+    //     dni: "45678912",
+    //     rol: "tecnico_electrico",
+    //     empresa: "Interno",
+    //   },
+    //   {
+    //     nombre: "Mario",
+    //     apellido: "Ramírez",
+    //     dni: "45678913",
+    //     rol: "tecnico_electrico",
+    //     empresa: "Interno",
+    //   },
+    //   {
+    //     nombre: "Lucio",
+    //     apellido: "Fernández",
+    //     dni: "45678914",
+    //     rol: "tecnico_electrico",
+    //     empresa: "Interno",
+    //   },
+    //   {
+    //     nombre: "Victor",
+    //     apellido: "Santos",
+    //     dni: "45678915",
+    //     rol: "tecnico_electrico",
+    //     empresa: "Interno",
+    //   },
+    //   {
+    //     nombre: "Aldair",
+    //     apellido: "Mendoza",
+    //     dni: "45678916",
+    //     rol: "tecnico_electrico",
+    //     empresa: "Interno",
+    //   },
 
-      {
-        nombre: "Carlos",
-        apellido: "Gómez",
-        dni: "47891234",
-        rol: "tecnico_mecanico",
-        empresa: "Interno",
-      },
-      {
-        nombre: "Junior",
-        apellido: "Vargas",
-        dni: "47891235",
-        rol: "tecnico_mecanico",
-        empresa: "Interno",
-      },
-      {
-        nombre: "Eduardo",
-        apellido: "Silva",
-        dni: "47891236",
-        rol: "tecnico_mecanico",
-        empresa: "Interno",
-      },
-      {
-        nombre: "Miguel",
-        apellido: "Hernández",
-        dni: "47891237",
-        rol: "tecnico_mecanico",
-        empresa: "Interno",
-      },
-      {
-        nombre: "Oscar",
-        apellido: "Ramírez",
-        dni: "47891238",
-        rol: "tecnico_mecanico",
-        empresa: "Interno",
-      },
+    //   {
+    //     nombre: "Carlos",
+    //     apellido: "Gómez",
+    //     dni: "47891234",
+    //     rol: "tecnico_mecanico",
+    //     empresa: "Interno",
+    //   },
+    //   {
+    //     nombre: "Junior",
+    //     apellido: "Vargas",
+    //     dni: "47891235",
+    //     rol: "tecnico_mecanico",
+    //     empresa: "Interno",
+    //   },
+    //   {
+    //     nombre: "Eduardo",
+    //     apellido: "Silva",
+    //     dni: "47891236",
+    //     rol: "tecnico_mecanico",
+    //     empresa: "Interno",
+    //   },
+    //   {
+    //     nombre: "Miguel",
+    //     apellido: "Hernández",
+    //     dni: "47891237",
+    //     rol: "tecnico_mecanico",
+    //     empresa: "Interno",
+    //   },
+    //   {
+    //     nombre: "Oscar",
+    //     apellido: "Ramírez",
+    //     dni: "47891238",
+    //     rol: "tecnico_mecanico",
+    //     empresa: "Interno",
+    //   },
 
-      {
-        nombre: "Roy",
-        apellido: "García",
-        dni: "48912345",
-        rol: "operario_de_mantenimiento",
-        empresa: "Contratista SAC",
-      },
-      {
-        nombre: "Luis",
-        apellido: "Torres Medina",
-        dni: "48912346",
-        rol: "operario_de_mantenimiento",
-        empresa: "Contratista SAC",
-      },
-      {
-        nombre: "Jean",
-        apellido: "Perez",
-        dni: "48912347",
-        rol: "operario_de_mantenimiento",
-        empresa: "Contratista SAC",
-      },
-      {
-        nombre: "Andres",
-        apellido: "Gonzalez",
-        dni: "48912348",
-        rol: "operario_de_mantenimiento",
-        empresa: "Contratista SAC",
-      },
-      {
-        nombre: "Sebastian",
-        apellido: "Nuñez",
-        dni: "48912349",
-        rol: "supervisor",
-        empresa: "Contratista SAC",
-      },
-    ];
+    //   {
+    //     nombre: "Roy",
+    //     apellido: "García",
+    //     dni: "48912345",
+    //     rol: "operario_de_mantenimiento",
+    //     empresa: "Contratista SAC",
+    //   },
+    //   {
+    //     nombre: "Luis",
+    //     apellido: "Torres Medina",
+    //     dni: "48912346",
+    //     rol: "operario_de_mantenimiento",
+    //     empresa: "Contratista SAC",
+    //   },
+    //   {
+    //     nombre: "Jean",
+    //     apellido: "Perez",
+    //     dni: "48912347",
+    //     rol: "operario_de_mantenimiento",
+    //     empresa: "Contratista SAC",
+    //   },
+    //   {
+    //     nombre: "Andres",
+    //     apellido: "Gonzalez",
+    //     dni: "48912348",
+    //     rol: "operario_de_mantenimiento",
+    //     empresa: "Contratista SAC",
+    //   },
+    //   {
+    //     nombre: "Sebastian",
+    //     apellido: "Nuñez",
+    //     dni: "48912349",
+    //     rol: "supervisor",
+    //     empresa: "Contratista SAC",
+    //   },
+    // ];
 
-    for (const t of trabajadores) {
-      await Trabajador.findOrCreate({
-        where: { dni: t.dni },
-        defaults: t,
-      });
-    }
+    // for (const t of trabajadores) {
+    //   await Trabajador.findOrCreate({
+    //     where: { dni: t.dni },
+    //     defaults: t,
+    //   });
+    // }
 
-    console.log("👷 Trabajadores OK");
+    // console.log("👷 Trabajadores OK");
 
     console.log("🎉 SEED COMPLETADO CORRECTAMENTE");
     process.exit(0);
